74cee20f7948045d702db0e5fea98c5803da2273
Alessandro De Marco <info@alessandrodemarco.net>
2024-03-31 02:31:46 +0100

    Disabled the ability to remove / rename readonly files/folders

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -27,7 +27,7 @@
            v-if="app.features.includes(FEATURES.RENAME)"
            @click="(selectedItems.length != 1) || emit('vf-rename', selectedItems)">
         <svg xmlns="http://www.w3.org/2000/svg"
-             :class="(selectedItems.length == 1) ? 'cursor-pointer stroke-gray-500 hover:stroke-cyan-700 dark:stroke-gray-400 dark:hover:stroke-gray-300' : 'stroke-gray-200  dark:stroke-gray-700'"
+             :class="(selectedItems.length == 1 && !selectedItems[0].readonly) ? 'cursor-pointer stroke-gray-500 hover:stroke-cyan-700 dark:stroke-gray-400 dark:hover:stroke-gray-300' : 'stroke-gray-200  dark:stroke-gray-700'"
              class="h-6 w-6 md:h-8 md:w-8 m-auto" fill="none" viewBox="0 0 24 24" stroke="none" stroke-width="1.5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
         </svg>
@@ -38,7 +38,7 @@
            v-if="app.features.includes(FEATURES.DELETE)"
            @click="(!selectedItems.length) || emit('vf-delete', selectedItems)">
         <svg xmlns="http://www.w3.org/2000/svg"
-             :class="(selectedItems.length) ? 'cursor-pointer stroke-gray-500 hover:stroke-cyan-700 dark:stroke-gray-400 dark:hover:stroke-gray-300' : 'stroke-gray-200  dark:stroke-gray-700'"
+             :class="(selectedItems.length && canDelete()) ? 'cursor-pointer stroke-gray-500 hover:stroke-cyan-700 dark:stroke-gray-400 dark:hover:stroke-gray-300' : 'stroke-gray-200  dark:stroke-gray-700'"
              class="h-6 w-6 md:h-8 md:w-8 m-auto" fill="none" viewBox="0 0 24 24" stroke="none" stroke-width="1.5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
         </svg>
@@ -141,6 +141,10 @@ const toggleFullScreen = () => {
   app.fullScreen = !app.fullScreen;
   setStore('full-screen', app.fullScreen);
   app.emitter.emit('vf-fullscreen-toggle');
+}
+
+const canDelete = () => {
+  return !selectedItems.value.some(item => item.readonly);
 }
 
 app.emitter.on('vf-nodes-selected', (items) => {
